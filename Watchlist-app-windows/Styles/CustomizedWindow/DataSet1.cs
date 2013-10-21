03d4b21d51ac9775a9b668ac3fbdbcda3784efa6
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2013-10-21 10:55:31 +0300

    Add data schema

---
+++ Watchlist-app-windows/Styles/CustomizedWindow/DataSet1.cs
@@ -0,0 +1,21 @@
+﻿namespace Watchlist_app_windows.Styles.CustomizedWindow {
+    
+    
+    public partial class DataSet1 {
+        partial class MovieListDataTable
+        {
+        }
+    
+        partial class UserDataTable
+        {
+        }
+    
+        partial class ListDataTable
+        {
+        }
+    
+        partial class MovieDataTable
+        {
+        }
+    }
+}
